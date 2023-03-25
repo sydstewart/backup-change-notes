5ca6892a4473639f8e9d252d4559c337f13399ed
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-03-25 17:48:56 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.media
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +12,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+     
+    result = anvil.server.call("export_to_excel") #, <pass your data and columns here>)
+    anvil.media.download(result)
+    pass
+
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,36 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+import anvil.media
+from anvil.tables import app_tables
+import anvil.server
+import io
+import pandas as pd
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+@anvil.server.callable
+def create_excel():
+ 
+  print(df)
+  change_note_excel = df.to_excel('change_note_excel.xlsx')
+  # print(change_note_excel)
+
+
+@anvil.server.callable
+def export_to_excel():
+     all_change_notes= app_tables.change_notes.search()
+ 
+# For each row, pull out only the data we want to put into pandas
+     dicts = [{ 'title': r['title']}#, 'change_date': r['change_date']}
+         for r in all_change_notes]
+#'new_change_note_id': r['new_change_note_id'],
+     df = pd.DataFrame.from_dict(dicts)
+     # df = pd.DataFrame(data, columns=columns)
+     content = io.BytesIO()
+     df.to_excel(content, index=False)
+     content.seek(0, 0)
+     return anvil.BlobMedia(content=content.read(), content_type="application/vnd.ms-excel")
