cb471e89b6ed09f64e8325e8eff87db6bd1a9cce
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-03-26 10:36:31 +0000

    Edited ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -5,8 +5,11 @@ import anvil.tables.query as q
 import anvil.media
 from anvil.tables import app_tables
 import anvil.server
+import anvil.tz
 import io
 import pandas as pd
+from datetime import datetime, time , date , timedelta, timezone
+
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
@@ -23,16 +26,23 @@ def create_excel():
 
 
 @anvil.server.callable
-def export_to_excel():
-     all_change_notes= app_tables.change_notes.search()
- 
-# For each row, pull out only the data we want to put into pandas
-     dicts = [{'new_change_note_id': r['new_change_note_id'], 'title': r['title']}#, 'change_date': r['change_date']}
-         for r in all_change_notes]
-#'],
-     df = pd.DataFrame.from_dict(dicts)
-     # df = pd.DataFrame(data, columns=columns)
-     content = io.BytesIO()
-     df.to_excel(content, index=False)
-     content.seek(0, 0)
-     return anvil.BlobMedia(content=content.read(), content_type="application/vnd.ms-excel" , name = 'Change_note.xlsx')
+def export_to_csv():
+     # all_change_notes= app_tables.change_notes.search()
+     # all_change_note_audits =app_tables.change_notes_audit.search()
+     
+  
+     change_note_csv = app_tables.change_notes.search().to_csv()
+     change_note_audit_csv = app_tables.change_notes_audit.search().to_csv()
+     users_csv = app_tables.users.search().to_csv()
+     return change_note_csv, change_note_audit_csv, users_csv
+     # dicts = [{'new_change_note_id': r['new_change_note_id'], 
+     #           'title': r['title'],
+     #           'change_date': r['change_date'].replace(tzinfo=None)}       #datetimeObject = datetimeObject.replace(tzinfo=None)
+     #     for r in all_change_notes]
+   
+     # df = pd.DataFrame.from_dict(dicts)
+     # # df = pd.DataFrame(data, columns=columns)
+     # content = io.BytesIO()
+     # df.to_excel(content, index=False)
+     # content.seek(0, 0)
+     # return anvil.BlobMedia(content=content.read(), content_type="application/vnd.ms-excel" , name = 'Change_note.xlsx')
